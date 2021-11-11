31563b48bd8bdc4105b83d8ee613301bbf9b9931
Hytrex <86927102+iHytrex@users.noreply.github.com>
2021-11-10 21:56:55 -0300

    Create CS.cs

---
+++ CS.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWord;
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word!");
+        }
+    }
+}
